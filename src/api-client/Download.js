2a7bde94ad6eeca996dc859208ffed2194187f92
Maryam Saeedmehr <6520696-BitterOcean@users.noreply.gitlab.com>
2021-06-18 14:51:53 +0430

    fix Download problem

--- src/api-client/Download.js
+++ src/api-client/Download.js
@@ -1,33 +1,12 @@
 import { API_DOWNLOAD_FILE } from '../apiConstants';
 
 const download = async (file_id, setSnackbarInfo) => {
-  fetch(`${API_DOWNLOAD_FILE}?token=${localStorage.token}&file_id=${file_id}`, {
-    method: 'get',
-    headers: { 'Content-Type': 'application/json' },
-  })
-    .then(response => response.json())
-    .then((responseJson) => {
-      if (responseJson.code === 200) {
-        setSnackbarInfo({
-          open: true,
-          message: 'فایل با موفقیت دانلود شد',
-          severity: 'success',
-        });
-      } else {
-        setSnackbarInfo({
-          open: true,
-          message: 'در بارگیری فایل خطایی رخ داده است. لطفاً مجدداً تلاش کنید',
-          severity: 'error',
-        });
-      }
-    })
-    .catch(() => {
-      setSnackbarInfo({
-        open: true,
-        message: 'در ارتباط با سرور خطایی رخ داده است. لطفاً مجدداً تلاش کنید',
-        severity: 'error',
-      });
-    });
+  window.location.href = `${API_DOWNLOAD_FILE}?token=${localStorage.token}&file_id=${file_id}`;
+  setSnackbarInfo({
+    open: true,
+    message: 'فایل با موفقیت دانلود شد',
+    severity: 'success',
+  });
 };
 
 export default download;
